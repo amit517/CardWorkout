f1857cbd457376576b003dd0b8b4fa00c52fe48f
Amit Kundu <kundu.amit517@gmail.com>
2024-04-01 00:16:47 +0300

    IBOutlet reference moved to array and added few more click action listener

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class CardSelectionVC: UIViewController {
     @IBOutlet var cardImageView: UIImageView!
-    @IBOutlet var stopButton: UIButton!
-    @IBOutlet var rulesButton: UIButton!
-    @IBOutlet var restartButton: UIButton!
-    
     @IBOutlet var buttons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        stopButton.layer.cornerRadius = 8
-        rulesButton.layer.cornerRadius = 8
-        restartButton.layer.cornerRadius = 8
+//        stopButton.layer.cornerRadius = 8
+//        rulesButton.layer.cornerRadius = 8
+//        restartButton.layer.cornerRadius = 8
+        
+        for button in buttons{
+            button.layer.cornerRadius = 8
+        }
     }
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
         
     }
     
+    @IBAction func restartButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+    }
 }
